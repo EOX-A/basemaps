fd7541b3bb027c115768d556025943cf7f9ab126
Joachim Ungar <joachim.ungar@eox.at>
2013-11-22 15:40:19 +0100

    customized imposm mapping

--- imposm-mapping.py
+++ imposm-mapping.py
@@ -42,7 +42,7 @@ from imposm.mapping import (
 # imposm_linestring_max_length = 50
 
 
-# set_default_name_type(LocalizedName(['name:en', 'int_name', 'name']))
+set_default_name_type(LocalizedName(['name:en', 'int_name', 'name']))
 
 db_conf = Options(
     db='eox_osm',
@@ -97,6 +97,39 @@ places = Points(
             'locality',
         ])),
         ('population', Integer()),
+    ),
+)
+
+natural_points = Points(
+    name = 'natural_points',
+    mapping = {
+        'natural': (
+            'stone',
+            'tree',
+            'spring',
+            'peak',
+            'saddle',
+            'volcano',
+        ),
+    },
+    fields = (
+        ('name', String()),
+        ('ele', Integer()),
+        ('status', String()),
+        ('type', String()),
+    ),
+)
+
+natural_lines = LineStrings(
+    name = 'natural_lines',
+    mapping = {
+        'natural': (
+            'arete',
+            'ridge',
+        ),
+    },
+    fields = (
+        ('name', String()),
     ),
 )
 
@@ -324,6 +357,11 @@ landusages = Polygons(
             'wood',
             'land',
             'scrub',
+            'beach',
+            'glacier',
+        ),
+        'geological': (
+            'moraine',
         ),
         'highway': (
             'pedestrian',
